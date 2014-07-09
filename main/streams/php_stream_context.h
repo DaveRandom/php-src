03ea6bd02e9f5a1151ea44ab9a0b1b4a8ef4a07b
Chris Wright <daverandom@php.net>
2014-07-09 18:50:34 +0100

    I suck

--- main/streams/php_stream_context.h
+++ main/streams/php_stream_context.h
@@ -53,7 +53,7 @@ struct _php_stream_context {
 BEGIN_EXTERN_C()
 PHPAPI void php_stream_context_free(php_stream_context *context);
 PHPAPI php_stream_context *php_stream_context_alloc(TSRMLS_D);
-PHPAPI void php_stream_context_hydrate(php_stream_context *context, zval *options, zval *params TSRMLS_DC);
+PHPAPI int php_stream_context_hydrate(php_stream_context *context, zval *options, zval *params TSRMLS_DC);
 PHPAPI php_stream_context *_php_stream_context_from_zval(zval *zcontext, int nodefault TSRMLS_DC);
 PHPAPI int php_stream_context_get_option(php_stream_context *context,
 		const char *wrappername, const char *optionname, zval ***optionvalue);
